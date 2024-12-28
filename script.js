1df4a8286f74f95e9bbe5a7d51a98284f168c5db
Payal Talwekar <49368087+Payal0810@users.noreply.github.com>
2024-12-28 08:00:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const divEle = document.querySelector('.card-container');
+
+function getDetails(id){
+  const req = new XMLHttpRequest();
+
+  req.open('GET', `https://dummyjson.com/users/${id}`);
+  req.send();
+
+  req.addEventListener('load', function () {
+    const data = JSON.parse(this.responseText);
+    displayUser(data);
+  });
+}
+
+  function displayUser(data){
+    const card = `<div class="user-card">
+      <img src=${data.image} alt="Profile Image" />
+      <h3>${data.firstName}</h3>
+      <h3>${data.lastName}</h3>
+      <p class="email">${data.email}</p>
+      <button class="btn">View Profile</button>
+      </div>`;
+
+      divEle.insertAdjacentHTML('beforeend', card);
+  }
+
+  getDetails(56);
+
+  
